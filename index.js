c1841dc4489cc1948b8eae47f7e2338e2a004b58
Marco Troisi <hello@marcotroisi.com>
2017-07-25 09:11:27 +0100

    MTR-2 Mongo queries (find and insert)

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const _ = require('lodash');
-const R = require('ramda');
+const DB = require('./src/DB')();
+
+// DB.insertTranslation('hello', 'hallo', 'de_DE');
+
+var res = DB.readTranslation('hello', 'de_DE');
+
+res.then(function(r) { console.log(r)} );
--- src/DB.js
+++ src/DB.js
@@ -1,9 +1,37 @@
 const MongoClient = require('mongodb');
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
 
 module.exports = function(context) {
-  const connect = async function() {
-    const db = MongoClient.connect('mongodb://localhost:27017/microtranslator');
+  /**
+   * @return {Promise.<*>}
+   */
+  const connect = function() {
+    return MongoClient.connect('mongodb://localhost:27017/microtranslator').
+      then((db) => {
+        return db;
+      }).
+      catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return {
+    insertTranslation: function(word, translation, locale) {
+      return connect().then((db) => {
+        return db.collection('translations').insertOne({
+          word, translation, locale,
+        }).then((res) => {
+          console.log(res);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+    readTranslation: function(word, locale) {
+      return connect().then((db) => {
+        return db.collection('translations').findOne({
+          word, locale,
+        });
+      });
+    },
   };
 };
